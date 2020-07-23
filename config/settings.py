417937317512a9e2e59f7333718cc07615d94317
alexjeman <contact@alexjeman.com>
2020-07-23 10:58:14 +0300

    Fix static root path

--- config/settings.py
+++ config/settings.py
@@ -145,12 +145,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# NGINX - alias /var/path/to/basedir/static/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Swagger
 SWAGGER_SETTINGS = {
